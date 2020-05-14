bca23d69ee7c7bb834750542d4274dfb4a816465
Taylor Morris <tmorris@spoton.com>
2020-05-14 15:21:41 -0500

    Basic code-splitting. No content changes yet.

---
+++ thesoftwareartisan/components/globalhead.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+
+export default function GlobalHead () {
+    return (<Head>
+        <title>The Software Artisan</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>);
+}
